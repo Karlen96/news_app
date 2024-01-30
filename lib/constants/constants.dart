2bb095ab0f7e89a5ea66341ae9454f57968e8a2b
Karlen96 <59868342+Karlen96@users.noreply.github.com>
2024-01-30 10:04:53 +0400

    fit: Clerify code

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,4 +3,3 @@ import 'package:flutter/material.dart';
 const appName = 'News App';
 const supportedLocales = Locale('en', 'US');
 const featuredItemsHeight = 84.0;
-
--- lib/constants/dimens.dart
+++ lib/constants/dimens.dart
@@ -1,4 +1,3 @@
-
 const double kHorizontalPaddingValueSmall = 16;
 const double kVerticalPaddingValue = 12;
 const double kHorizontalPaddingValueLarge = 24;
